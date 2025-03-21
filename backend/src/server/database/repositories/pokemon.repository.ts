94eb7629da8a4c939800fc6034aeefd3e72d03f4
Rafa Linares <rafa.linares.molina@gmail.com>
2025-03-21 12:56:39 +0100

    refactor: added a repository to avoid use directly the quequelize model of Pokemon

---
+++ backend/src/server/database/repositories/pokemon.repository.ts
@@ -0,0 +1,44 @@
+import { FindOptions } from "sequelize";
+import { PokemonDto } from "../../models/Pokemon.dto";
+import Pokemon from "../models/pokemon";
+import { plainToInstance } from "class-transformer";
+import tryCatch from "../../../utils/tryCatch";
+
+class PokemonRepository {
+  async findAll(options: FindOptions<PokemonDto> = {}): Promise<PokemonDto[]> {
+    const { data: payload, error } = await tryCatch(Pokemon.findAll(options));
+
+    if (error) {
+      console.error("Error in PokemonRepository.findAll:", error);
+      return [];
+    }
+
+    const plainPokemon = payload.map((pokemon) => pokemon.get({ plain: true }));
+    const pokemon = plainToInstance(PokemonDto, plainPokemon, {
+      excludeExtraneousValues:true
+    });
+    return pokemon;
+  }
+
+  async create(pokemonData: PokemonDto): Promise<PokemonDto> {
+    const { data: rawPokemon, error } = await tryCatch(
+      Pokemon.create({
+        name: pokemonData.name,
+        height: pokemonData.height,
+        number: pokemonData.number,
+        health: pokemonData.health,
+        weight: pokemonData.weight,
+        url: pokemonData.url,
+      })
+    );
+
+    if (error) {
+      console.error("Error in PokemonRepository.create:", error);
+    }
+
+    const pokemon = plainToInstance(PokemonDto, rawPokemon);
+    return pokemon;
+  }
+}
+
+export default new PokemonRepository();
